84d59208b5de58643855644cdee1d02f2fe46a89
y33chan <y33chan@uwaterloo.ca>
2024-05-13 01:49:53 -0400

    Fixed Functionality file, updated srcs

--- Functionality.js
+++ Functionality.js
@@ -1,40 +1,46 @@
 
 //parameter definition for function
 let section = ""
-//modularizing function to scroll
-ScrollClick(section)
+// function to scroll
+function ScrollClick(section)
 {
-    document.getElementById(section).scrollIntoView({
+    document.getElementById(section).scrollIntoView
+    ({
         behavior: "smooth", block: "center", inline: "center"
     });
 }
 
-// Listens for the "Experience" button click
-// document.querySelector("button").addEventListener("click", function()
-
-document.getElementById("Experience").addEventListener("click", function()
+// BUTTONS SMOOTH SCROLL
+// Listens for the DOM to be fully oaded
+document.addEventListener("DOMContentLoaded", function() 
 {
-    // parameter is the section title Div ID
-    ScrollClick("ScrollExperience");
+    // Listens for the "Experience" button click
+    document.getElementById("Experience").addEventListener("click", function()
+    {
+        // parameter is the section title Div ID
+        ScrollClick("ScrollExperience");
+    });
+
+    // Listens for the "Projects" button click
+    document.getElementById("Projects").addEventListener("click", function()
+    {
+        // parameter is the section title Div ID
+        ScrollClick("ScrollProjects");
+    });
+
+    // Listens for the "Photography" button click
+    document.getElementById("Photography").addEventListener("click", function()
+    {
+        // parameter is the section title Div ID
+        // sends to endpoint of photography page, needs a back button but later implementtion
+    });
+
+    // Listens for the "Contact" button click
+    document.getElementById("Contact").addEventListener("click", function()
+    {
+        // parameter is the section title Div ID
+        ScrollClick("ScrollContact");
+    });
 });
 
-// Listens for the "Projects" button click
-document.getElementById("Projects").addEventListener("click", function()
-{
-    // parameter is the section title Div ID
-    ScrollClick("ScrollProjects");
-});
 
-// Listens for the "Photography" button click
-document.getElementById("Photography").addEventListener("click", function()
-{
-    // parameter is the section title Div ID
-    // sends to endpoint of photography page, needs a back button but later implementtion
-});
-
-// Listens for the "Contact" button click
-document.getElementById("Contact").addEventListener("click", function()
-{
-    // parameter is the section title Div ID
-    ScrollClick("ScrollProjects");
-});
