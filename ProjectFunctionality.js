e971a3dc7f428666b8d3e3541576be9b76d4b4ab
y33chan <y33chan@uwaterloo.ca>
2024-05-26 02:11:41 -0400

    More reorganized files, began creating project html, css, js files

---
+++ ProjectFunctionality.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() 
+{
+    // Listens for the "AboutMe" button click
+    document.getElementById("HomeButton").addEventListener("click", function()
+    {
+        
+    });
+});
